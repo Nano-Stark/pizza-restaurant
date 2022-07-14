3acf3bc3358bc46c790cbe937eb229cd866c9ed8
Nano-Stark <inventorstark7@gmail.com>
2022-07-14 21:02:02 +0100

    Integrating vercel with mongodb

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // const MONGO_URL = process.env.MONGO_URL
-const MONGO_URL = process.env.MONGOBD_URI;
+const MONGO_URL = process.env.MONGODB_URI;
 
 if (!MONGO_URL) {
   throw new Error(

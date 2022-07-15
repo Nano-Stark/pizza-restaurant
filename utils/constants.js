a474d09e238d00686b2ca00c13a8a6bdb61ccaef
Nano-Stark <inventorstark7@gmail.com>
2022-07-15 07:05:00 +0100

    Integrating netlify with mongodb

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 let BASE_URL;
 if (process.env.NODE_ENV === "production") {
-  // BASE_URL = "https://pizza-restuarant.netlify.app";
-  BASE_URL = "https://pizza-restaurant-phi.vercel.app";
+  BASE_URL = "https://pizza-restuarant.netlify.app";
+  // BASE_URL = "https://pizza-restaurant-phi.vercel.app";
 } else {
   BASE_URL = "http://localhost:3000";
 }
